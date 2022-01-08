237379247c63942c8365ad7d71cd9d22657cd465
Robert Corlett <bob@corlett.io>
2022-01-07 21:48:40 -1000

    Fetch an ipv6 address instead of ipv4

--- src/lib/ip-address.js
+++ src/lib/ip-address.js
@@ -1,5 +1,7 @@
+// updated to fetch the device's current public ipv6 address
+
 const axios = require('axios').default
-const GET_JSON_IP_URL = 'https://api.ipify.org?format=json'
+const GET_JSON_IP_URL = 'https://api64.ipify.org?format=json'
 
 module.exports = async (command) => {
   let ipAddress = null
